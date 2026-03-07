155f1863092c3a9ff211f54a0d1eb7aeeb44756d
Singampalli Venu Vardhan <147712505+venu-vardhan@users.noreply.github.com>
2026-03-07 21:44:21 +0530

    Time: 20 ms (66.23%) | Memory: 45.7 MB (77.10%) - LeetSync

---
+++ 18-4sum/4sum.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length < 4) return res;
+        Arrays.sort(nums); // Sort for efficiency
+        int n = nums.length;
+        
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                int l = j + 1, r = n - 1;
+                while (l < r) {
+                    long sum = (long) nums[i] + nums[j] + nums[l] + nums[r];
+                    if (sum == target) {
+                        res.add(Arrays.asList(nums[i], nums[j], nums[l], nums[r]));
+                        while (l < r && nums[l] == nums[l + 1]) l++;
+                        while (l < r && nums[r] == nums[r - 1]) r--;
+                        l++; r--;
+                    } else if (sum < target) l++;
+                    else r--;
+                }
+            }
+        }
+        return res;
+    }
+}

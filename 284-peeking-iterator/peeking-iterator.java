32df97ff37c8f5b175232490bc811cd8b49dc49a
Singampalli Venu Vardhan <147712505+venu-vardhan@users.noreply.github.com>
2026-04-08 22:09:02 +0530

    Time: 4 ms (82.62%) | Memory: 43.8 MB (36.70%) - LeetSync

---
+++ 284-peeking-iterator/peeking-iterator.java
@@ -0,0 +1,28 @@
+class PeekingIterator implements Iterator<Integer> {
+  public PeekingIterator(Iterator<Integer> iterator) {
+    this.iterator = iterator;
+    buffer = iterator.hasNext() ? iterator.next() : null;
+  }
+
+  // Returns the next element in the iteration without advancing the iterator.
+  public Integer peek() {
+    return buffer;
+  }
+
+  // hasNext() and next() should behave the same as in the Iterator interface.
+  // Override them if needed.
+  @Override
+  public Integer next() {
+    Integer next = buffer;
+    buffer = iterator.hasNext() ? iterator.next() : null;
+    return next;
+  }
+
+  @Override
+  public boolean hasNext() {
+    return buffer != null;
+  }
+
+  private Iterator<Integer> iterator;
+  private Integer buffer;
+}
